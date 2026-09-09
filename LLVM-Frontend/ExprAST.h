400d1377ad6865adcb14fdb21cec40ed8fdeb829
thebadgmaemine-droid <thebadgmaemine@gmail.com>
2026-09-10 00:00:40 +0700

    Seperated parser headerAdd buildPresets to select LLVM-Frontend as startup target

--- LLVM-Frontend/ExprAST.h
+++ LLVM-Frontend/ExprAST.h
@@ -116,10 +116,8 @@ public:
     FunctionExprAST(const std::string& Calee,
         std::vector<std::unique_ptr<ExprAST>> Args)
         : Calee(Calee), Args(std::move(Args)) {
-
     }
 };
-
 class PrototypeAST : public ExprAST {
     const std::string name;
     const std::vector<std::string> Args; // Initialize with no value b
@@ -128,10 +126,7 @@ public:
         : name(name), Args(std::move(Args)) {
     } // Implement prototype constructor 
 // okay sorry , std::move cast argument to an rvalue ( if you remember, is essential non-guarded memory ) reference -> compiler can steal resources all i wants
-
-
 };
-
 class FunctionAST : public ExprAST {
     std::unique_ptr<PrototypeAST> Proto;
     std::unique_ptr<ExprAST> Body;
@@ -139,157 +134,7 @@ public:
     FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body)
         : Proto(std::move(Proto)), Body(std::move(Body)) {
     }
-
 };
-
-
-// Parser
-static int Curtok; // Current token, parser and lexer looks at this
-static int getNextToken() {
-    return Curtok = gettok();  // Don't shadow value Curtok
-}     // Goes to next token
-
-// Binop precedence : Holds the precedence for Binary values 
-static std::map<char, int> BinopPrecedence;
-static int GetTokPrecedence() {
-    if (!isascii(Curtok))
-        return -1;
-    // Delcared -> safe
-    int TokPrec = BinopPrecedence[Curtok]; // Cannot be 0 ( default value ), so is either negative or positive ( negative acts as return -1; and postiive acts as return n;
-    if (TokPrec <= 0)
-        return -1;
-    return TokPrec;
-}
-
-/* parse_expression()
-    return parse_expression_1(parse_primary(), 0)
-parse_expression_1(lhs, min_precedence)
-    lookahead := peek next token
-    while lookahead is a binary operator whose precedence is >= min_precedence
-        op := lookahead
-        advance to next token
-        rhs := parse_primary ()
-        lookahead := peek next token
-        while lookahead is a binary operator whose precedence is greater
-                 than op's, or a right-associative operator
-                 whose precedence is equal to op's
-            rhs := parse_expression_1 (rhs, precedence of op + (1 if lookahead precedence is greater, else 0))
-            lookahead := peek next token
-        lhs := the result of applying op with operands lhs and rhs
-    return lhs */
-    // Def
-static std::unique_ptr<ExprAST> ParseExpression();
-static std::unique_ptr<ExprAST> ParsePrimary();
-static std::unique_ptr<ExprAST> LogError(const char* Str);
-static std::unique_ptr<ExprAST> ParseIdentifierExpr();
-
-static std::unique_ptr<ExprAST> ParseNumberExpr() {
-    auto Result = std::make_unique<NumberExprAST>(NumVal);
-    getNextToken();
-    return std::move(Result);
-}
-
-static std::unique_ptr<ExprAST> ParseParentExpr() {
-    getNextToken();
-    auto V = ParseExpression();
-    if (!V) {
-        return nullptr;
-    }
-    if (Curtok != ')') {
-        LogError("Expected )");
-    }
-
-
-}
-
-// Error logging
-std::unique_ptr<ExprAST> LogError(const char* Str) {
-    std::cerr << "Error:" << Str << "\n";
-    return nullptr;
-
-}
-// std::unique_ptr<ExprAST> LogError(const char *Str)
-// Primary
-// def
-static std::unique_ptr<ExprAST> ParsePrimary() {
-    switch (Curtok) {
-    default:
-        return LogError("Expected an expression");
-    case (static_cast<int>(Token::tok_number)):
-        return ParseNumberExpr();
-    case (static_cast<int>(Token::tok_identifier)):
-        return ParseIdentifierExpr();
-    case 'C':
-        return ParseParentExpr();
-
-    }
-}
-
-static std::unique_ptr<ExprAST> ParseIdentifierExpr() {
-    std::string IdName = IdentifierStr;
-    getNextToken();
-    // For Calling
-    std::vector<std::unique_ptr<ExprAST>> Args;
-    if (Curtok != '(') {
-        return std::make_unique<VariableExprAST>(IdName);
-    }
-
-    if (Curtok != ')') {
-        while (true) {
-            if (auto Arg = ParseExpression()) {
-                Args.push_back(std::move(Arg));
-            }
-            else
-                return nullptr;
-
-
-            if (Curtok == ')')
-                break;
-
-            if (Curtok != ',')
-                return LogError("Expected ')' or ',' in argument list");
-            getNextToken();
-        }
-    }
-    getNextToken();
-    return std::make_unique<FunctionExprAST>(IdName, std::move(Args));
-}
-
-static std::unique_ptr<ExprAST> ParseExpression() {
-    return ParsePrimary();
-}
-
-
-static std::unique_ptr<ExprAST> ParseBinOpRHS(const int ExprPrec, std::unique_ptr<ExprAST> LHS) {
-    while (true) {
-        int TokPrec = GetTokPrecedence();
-        if (TokPrec < ExprPrec) {
-            return LHS;
-
-            int BinOp = Curtok;
-            getNextToken();
-            auto RHS = ParsePrimary();
-            if (!RHS) {
-                return nullptr;
-
-            }
-        }
-
-    }
-
-
-
-
-
-    /*
-    FunctionExprAST(const std::string &Calee,
-        std::vector<std::unique_ptr<ExprAST>> Args)
-            : Calee (Calee), Args(std::move(Args)) {
-    */
-    std::unique_ptr<ExprAST> LogErrorP(const char* Str) {
-        LogError(Str);
-        return nullptr;
-    }
 
     static std::unique_ptr<LLVMContext> TheContext;
     static std::unique_ptr<IRBuilder> Builder;
---
+++ LLVM-Frontend/parser.h
@@ -0,0 +1,132 @@
+#pragma once
+#ifndef parser_H_
+#define parser_H_
+#endif // !parser_H_
+
+using llvm;
+
+static int Curtok; // Current token, parser and lexer looks at this
+static int getNextToken() {
+    return Curtok = gettok();  // Don't shadow value Curtok
+}     // Goes to next token
+
+// Binop precedence : Holds the precedence for Binary values 
+static std::map<char, int> BinopPrecedence;
+static int GetTokPrecedence() {
+    if (!isascii(Curtok))
+        return -1;
+    // Delcared -> safe
+    int TokPrec = BinopPrecedence[Curtok]; // Cannot be 0 ( default value ), so is either negative or positive ( negative acts as return -1; and postiive acts as return n;
+    if (TokPrec <= 0)
+        return -1;
+    return TokPrec;
+}
+/* parse_expression()
+    return parse_expression_1(parse_primary(), 0)
+parse_expression_1(lhs, min_precedence)
+    lookahead := peek next token
+    while lookahead is a binary operator whose precedence is >= min_precedence
+        op := lookahead
+        advance to next token
+        rhs := parse_primary ()
+        lookahead := peek next token
+        while lookahead is a binary operator whose precedence is greater
+                 than op's, or a right-associative operator
+                 whose precedence is equal to op's
+            rhs := parse_expression_1 (rhs, precedence of op + (1 if lookahead precedence is greater, else 0))
+            lookahead := peek next token
+        lhs := the result of applying op with operands lhs and rhs
+    return lhs */
+    // Def
+static std::unique_ptr<ExprAST> ParseExpression();
+static std::unique_ptr<ExprAST> ParsePrimary();
+static std::unique_ptr<ExprAST> LogError(const char* Str);
+static std::unique_ptr<ExprAST> ParseIdentifierExpr();
+//-----------------------------FUNCTIONS DEF-------------------------------//
+static std::unique_ptr<ExprAST> ParseNumberExpr() {
+    auto Result = std::make_unique<NumberExprAST>(NumVal);
+    getNextToken();
+    return std::move(Result);
+}
+
+static std::unique_ptr<ExprAST> ParseParentExpr() {
+    getNextToken();
+    auto V = ParseExpression();
+    if (!V) {
+        return nullptr;
+    }
+    if (Curtok != ')') {
+        LogError("Expected )");
+    }
+}
+}
+static std::unique_ptr<ExprAST> ParsePrimary() {
+    switch (Curtok) {
+    default:
+        return LogError("Expected an expression");
+    case (static_cast<int>(Token::tok_number)):
+        return ParseNumberExpr();
+    case (static_cast<int>(Token::tok_identifier)):
+        return ParseIdentifierExpr();
+    case 'C':
+        return ParseParentExpr();
+
+    }
+}
+static std::unique_ptr<ExprAST> ParseIdentifierExpr() {
+    std::string IdName = IdentifierStr;
+    getNextToken();
+    // For Calling
+    std::vector<std::unique_ptr<ExprAST>> Args;
+    if (Curtok != '(') {
+        return std::make_unique<VariableExprAST>(IdName);
+    }
+
+    if (Curtok != ')') {
+        while (true) {
+            if (auto Arg = ParseExpression()) {
+                Args.push_back(std::move(Arg));
+            }
+            else
+                return nullptr;
+
+
+            if (Curtok == ')')
+                break;
+
+            if (Curtok != ',')
+                return LogError("Expected ')' or ',' in argument list");
+            getNextToken();
+        }
+    }
+    getNextToken();
+    return std::make_unique<FunctionExprAST>(IdName, std::move(Args));
+}
+static std::unique_ptr<ExprAST> ParseExpression() {
+    return ParsePrimary();
+}
+static std::unique_ptr<ExprAST> ParseBinOpRHS(const int ExprPrec, std::unique_ptr<ExprAST> LHS) {
+    while (true) {
+        int TokPrec = GetTokPrecedence();
+        if (TokPrec < ExprPrec) {
+            return LHS;
+
+            int BinOp = Curtok;
+            getNextToken();
+            auto RHS = ParsePrimary();
+            if (!RHS) {
+                return nullptr;
+
+            }
+        }
+
+    }
+    
+    FunctionExprAST(const std::string & Calee,
+        std::vector<std::unique_ptr<ExprAST>> Args)
+        : Calee(Calee), Args(std::move(Args)) {
+    }
+    std::unique_ptr<ExprAST> LogErrorP(const char* Str) {
+        LogError(Str);
+        return nullptr;
+    }
